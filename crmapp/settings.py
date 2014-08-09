2b154a981dd18a46ff5369f4ddb11f0a8a1f5d73
Tobias Abdon <tabdon@gmail.com>
2014-08-09 17:38:33 +0800

    Get Django Ready > Configure the Database

--- crmapp/settings.py
+++ crmapp/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'south',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -57,8 +58,12 @@ WSGI_APPLICATION = 'crmapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'crmeasyDB',
+        'USER': 'postgres',
+        'PASSWORD': 'crmcourse',
+        'HOST': '/tmp',
+        'PORT': '5432',
     }
 }
 
