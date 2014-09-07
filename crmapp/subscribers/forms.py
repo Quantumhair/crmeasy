cdd73635fc91947e32848f96ef21fe461de610c2
Tobias Abdon <tabdon@gmail.com>
2014-09-07 14:04:27 +0800

    Subscriber Form Part I > Create the Subscriber Form

---
+++ crmapp/subscribers/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+
+class SubscriberForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    last_name = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    email = forms.EmailField(
+        required=True, widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    username = forms.CharField(
+        widget=forms.TextInput(attrs={'class':'form-control'})
+    )
+    password1 = forms.CharField(
+        widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'})
+    )
+    password2 = forms.CharField(
+        widget=forms.TextInput(attrs={'class':'form-control', 'type':'password'})
+    )
