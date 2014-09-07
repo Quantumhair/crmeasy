89809ac0b80d417ada2b423fa746815c6ae9f8ed
Tobias Abdon <tabdon@gmail.com>
2014-09-07 19:08:47 +0800

    Contacts - Part II > Edit Contact - Modify View

--- crmapp/contacts/views.py
+++ crmapp/contacts/views.py
@@ -3,9 +3,11 @@ from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseForbidden
+from django.shortcuts import get_object_or_404
 
 from .models import Contact
 from .forms import ContactForm
+from crmapp.accounts.models import Account
 
 
 @login_required()
@@ -19,19 +21,24 @@ def contact_detail(request, uuid):
     )
 
 @login_required()
-def contact_cru(request):
+def contact_cru(request, uuid=None, account=None):
+
+    if uuid:
+        contact = get_object_or_404(Contact, uuid=uuid)
+        if contact.owner != request.user:
+            return HttpResponseForbidden()
+    else:
+        contact = Contact(owner=request.user)
 
     if request.POST:
-        form = ContactForm(request.POST)
+        form = ContactForm(request.POST, instance=contact)
         if form.is_valid():
             # make sure the user owns the account
             account = form.cleaned_data['account']
             if account.owner != request.user:
                 return HttpResponseForbidden()
             # save the data
-            contact = form.save(commit=False)
-            contact.owner = request.user
-            contact.save()
+            form.save()
             # return the user to the account detail view
             reverse_url = reverse(
                 'crmapp.accounts.views.account_detail',
@@ -39,10 +46,16 @@ def contact_cru(request):
             )
             return HttpResponseRedirect(reverse_url)
     else:
-        form = ContactForm()
+        form = ContactForm(instance=contact)
+
+    # this is used to render the account in the template
+    if request.GET.get('account', ''):
+        account = Account.objects.get(id=request.GET.get('account', ''))
 
     variables = {
         'form': form,
+        'contact': contact,
+        'account': account
     }
 
     template = 'contacts/contact_cru.html'
